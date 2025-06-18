8333a2f1f4d61553e0346541866a702bafffeeeb
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-18 12:02:45 +0530

    update code

--- src/main/java/edu/icet/ecom/Main.java
+++ src/main/java/edu/icet/ecom/Main.java
@@ -1,9 +1,47 @@
 package edu.icet.ecom;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         // Rock Papper Scissors game
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock","papper","scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain;
+
+        //player choice
+
+        System.out.print("Enter your move rock,papper,scissors : ");
+        playerChoice = scanner.nextLine().toLowerCase();
+
+        if (!playerChoice.equals("rock") &&
+                !playerChoice.equals("papper")&&
+                !playerChoice.equals("scissorts")){
+            System.out.println("Invalid choice");
+        }
+
+        //computer Choice
+
+        computerChoice = choices[random.nextInt(3)];
+        System.out.println("Computer choice :"+computerChoice);
+
+        if (playerChoice.equals(computerChoice)){
+            System.out.println("It's a Tie");
+        } else if (playerChoice.equals("rock") && computerChoice.equals("scissors")) {
+            System.out.println("You Win");
+        } else if ( ) {
+
+        } else {
+            System.out.println("You lose!");
+        }
+
 
         // 01. Daclare Variables
         // 02. Get Choice From User
