0dd2798d75f55c517e83b1d2454b5849c26372ea
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-18 11:40:01 +0530

    Inizialize

---
+++ src/main/java/edu/icet/ecom/Main.java
@@ -0,0 +1,16 @@
+package edu.icet.ecom;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Rock Papper Scissors game
+
+        // 01. Daclare Variables
+        // 02. Get Choice From User
+        // 03. Get Random Choice For The Computer
+        // 04. Check Win Conditions
+        // 05. Ask To Play Again ?
+        // 06. GoodBye message
+
+    }
+}
